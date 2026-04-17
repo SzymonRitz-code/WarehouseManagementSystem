a6ac679ec3ae5fa628e05c28357ff304f4b63e0e
Szymon Ritz <szymon.ritz@gmail.com>
2026-04-17 21:46:18 +0200

    json serialization improvements

--- WarehouseManagementSystem.API/Program.cs
+++ WarehouseManagementSystem.API/Program.cs
@@ -1,4 +1,6 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using System.Text.Json.Serialization;
 using WarehouseManagementSystem.API.Extensions;
 using WarehouseManagementSystem.API.Services.Documents;
 using WarehouseManagementSystem.API.Services.Queries;
@@ -11,8 +13,14 @@ using WarehouseManagementSystem.Infrastructure.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Dodałem NewtonssoftJson bo obługuje patchDocument Serializacja Enumów z tego powodu powinna być w nim dodana inaczej dojdzie do zgrzytu między dwoma konwerterami
+// Dodanie konwertera przy polu(w klasyczny sposób) nie jest wtedy obsługiwane.
+builder.Services.AddControllers().AddNewtonsoftJson(options =>
+{
+    //options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+    options.SerializerSettings.Converters.Add(new Newtonsoft.Json.Converters.StringEnumConverter());
+});
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -24,6 +32,7 @@ builder.Services.AddScoped<IDocumentCommandService, DocumentCommandService>();
 builder.Services.AddScoped<IGoodsIssueService, GoodsIssueService>();
 builder.Services.AddScoped<IGoodsReceiptService, GoodsReceiptService>();
 builder.Services.AddScoped<IStockTransferService, StockTransferService>();
+builder.Services.AddScoped<IProductBatchQueryService, ProductBatchQueryService>();
 
 
 builder.Services.AddScoped<IDocumentQueryService, DocumentQueryService>();
